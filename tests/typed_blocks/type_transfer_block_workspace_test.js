715fba7ec90bf18ee48d9fbbdf5d315bf1b6a12e
Haruka Matsumoto <harukam0416@gmail.com>
2018-11-06 18:19:20 +0900

    Add function to transfer headless block's workspace
    in transfer tests

--- tests/typed_blocks/type_transfer_block_workspace_test.js
+++ tests/typed_blocks/type_transfer_block_workspace_test.js
@@ -7,19 +7,28 @@ function create_typed_workspace() {
   return new Blockly.Workspace(workspaceOptions);
 }
 
+function virtually_transfer_workspace(block, targetWorkspace) {
+  var xml = Blockly.Xml.blockToDom(block);
+  var block = Blockly.Xml.domToBlock(xml, targetWorkspace);
+  return block;
+}
+
 function test_type_transfer_block_workspace_simpleValueBlock() {
   var workspace = create_typed_workspace();
   var otherWorkspace = create_typed_workspace()
   try {
-    var valueBlock = workspace.newBlock('let_typed');
-    var xml = Blockly.Xml.blockToDom(valueBlock);
+    var originalBlock = workspace.newBlock('let_typed');
+    var transferredBlock = virtually_transfer_workspace(originalBlock,
+        otherWorkspace);
+    var xml = Blockly.Xml.blockToDom(originalBlock);
     var block = Blockly.Xml.domToBlock(xml, otherWorkspace);
-    var value1 = valueBlock.getField('VAR').getVariable();
-    var value2 = valueBlock.getField('VAR').getVariable();
-    valueBlock.dispose();
+    var value1 = originalBlock.getField('VAR').getVariable();
+    var value2 = originalBlock.getField('VAR').getVariable();
+    originalBlock.setTransferring(true);
+    originalBlock.dispose();
     assertTrue(value1 == value2);
     // The following assertion would fail. The value no longer refers to the
-    // block because of valueBlock.dispose().
+    // block because of originalBlock.dispose().
     assertTrue(value2.getSourceBlock() == block);
   } finally {
     workspace.dispose();
