74502688a8df07485e77b34f86e2b5d5e3688402
Haruka Matsumoto <harukam0416@gmail.com>
2018-11-08 14:03:00 +0900

    Update function in transfer-tests according to
    changes in transferManager.placeNewBlock() in commit 121e0818
    
    The function in transfer-tests is expected to do the same thing as
    transferManager.placeNewBlock()

--- tests/typed_blocks/type_transfer_block_workspace_test.js
+++ tests/typed_blocks/type_transfer_block_workspace_test.js
@@ -7,12 +7,17 @@ function create_typed_workspace() {
   return new Blockly.Workspace(workspaceOptions);
 }
 
-function virtually_transfer_workspace(block, targetWorkspace) {
-  block.setTransferStatus(Blockly.TRANSFER_STATUS_ONGOING);
-  var xml = Blockly.Xml.blockToDom(block);
-  var block = Blockly.Xml.domToBlock(xml, targetWorkspace);
-  block.setTransferStatus(Blockly.TRANSFER_STATUS_DONE);
-  return block;
+/**
+ * Transfer block's workspace to the given workspace in the same way as
+ * placeNewBlock() in Blockly.WorkspaceTransferManager.
+ */
+function virtually_transfer_workspace(oldBlock, targetWorkspace) {
+  oldBlock.setTransferStatus(Blockly.TRANSFER_STATUS_ONGOING);
+  var xml = Blockly.Xml.blockToDom(oldBlock);
+  var newBlock = Blockly.Xml.domToBlock(xml, targetWorkspace);
+  oldBlock.setTransferStatus(Blockly.TRANSFER_STATUS_DONE);
+  oldBlock.dispose();
+  return newBlock;
 }
 
 function test_type_transfer_block_workspace_simpleValueBlock() {
@@ -20,11 +25,10 @@ function test_type_transfer_block_workspace_simpleValueBlock() {
   var otherWorkspace = create_typed_workspace()
   try {
     var originalBlock = workspace.newBlock('let_typed');
+    var value1 = originalBlock.getField('VAR').getVariable();
     var transferredBlock = virtually_transfer_workspace(originalBlock,
         otherWorkspace);
-    var value1 = originalBlock.getField('VAR').getVariable();
     var value2 = transferredBlock.getField('VAR').getVariable();
-    originalBlock.dispose();
     assertTrue(value1 != value2);
     assertTrue(value1.getId() != value2.getId());
   } finally {
@@ -47,7 +51,6 @@ function test_type_transfer_block_workspace_valueBlockWithReferences() {
     var transferredBlock = virtually_transfer_workspace(originalBlock,
         otherWorkspace);
     var transferredValue = transferredBlock.getField('VAR').getVariable();
-    originalBlock.dispose();
     // transferredBlock must have the identical variable value with the
     // original block.
     assertTrue(originalValue == transferredValue);
