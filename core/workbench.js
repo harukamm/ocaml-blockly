ce850eb491bbe6861f175af98e92889fbfbc9ed6
Haruka Matsumoto <harukam0416@gmail.com>
2018-11-20 16:43:08 +0900

    Disable create events for temporary blocks created
    to build language tree of workbench flyout

--- core/workbench.js
+++ core/workbench.js
@@ -375,17 +375,23 @@ Blockly.Workbench.prototype.getFlyoutLanguageTree_ = function() {
   var xml = goog.dom.createDom('xml');
   var env = this.getContext();
   var names = Object.keys(env);
-  for (var i = 0, name; name = names[i]; i++) {
-    var variable = env[name];
-    // TODO(harukam): Avoid providing prototype name using string literal.
-    var getterBlock = this.workspace_.newBlock('variables_get_typed');
-    var field = getterBlock.getField('VAR');
-    field.initModel();
-    field.setVariableName(name);
-    field.setBoundValue(variable);
-    var dom = Blockly.Xml.blockToDom(getterBlock);
-    getterBlock.dispose();
-    xml.appendChild(dom);
+
+  Blockly.Events.disable();
+  try {
+    for (var i = 0, name; name = names[i]; i++) {
+      var variable = env[name];
+      // TODO(harukam): Avoid providing prototype name using string literal.
+      var getterBlock = this.workspace_.newBlock('variables_get_typed');
+      var field = getterBlock.getField('VAR');
+      field.initModel();
+      field.setVariableName(name);
+      field.setBoundValue(variable);
+      var dom = Blockly.Xml.blockToDom(getterBlock);
+      getterBlock.dispose();
+      xml.appendChild(dom);
+    }
+  } finally {
+    Blockly.Events.enable();
   }
   return xml;
 };
