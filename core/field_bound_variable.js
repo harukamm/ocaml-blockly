73c44939227fbcff813316961b093832eeeaf247
Haruka Matsumoto <harukam0416@gmail.com>
2018-11-20 23:17:33 +0900

    Ignore events for field highlight if user is dragging

--- core/field_bound_variable.js
+++ core/field_bound_variable.js
@@ -386,7 +386,9 @@ Blockly.FieldBoundVariable.prototype.highlightVariables_ = function(on, e) {
   if (!this.variable_) {
     return;
   }
-  // TODO(harukam): Ignore events if the block is being dragged.
+  if (this.sourceBlock_.workspace.isDragging()) {
+    return;
+  }
   var variables = this.variable_.getAllBoundVariables();
   for (var i = 0, variable; variable = variables[i]; i++) {
     var field = variable.getContainerField();
