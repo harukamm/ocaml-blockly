01111bf561f3a3991f972aa43a73c660e92cbfd8
Haruka Matsumoto <harukam0416@gmail.com>
2018-12-07 21:39:24 +0900

    Make workspace cover the whole screen
    in demos/generator/typed

--- demos/generator/typed.js
+++ demos/generator/typed.js
@@ -5,6 +5,21 @@ var Typed = {};
 Typed.workspace = null;
 
 Typed.init = function() {
+  var onresize = function(e) {
+    var container = document.getElementById('workspaceArea');
+    var bBox = Typed.getBBox_(container);
+    var workspaceDiv = document.getElementById('blocklyDiv');
+    workspaceDiv.style.top = bBox.y + 'px';
+    workspaceDiv.style.left = bBox.x + 'px';
+    // Height and width need to be set, read back, then set again to
+    // compensate for scrollbars.
+    workspaceDiv.style.height = bBox.height + 'px';
+    workspaceDiv.style.height = (2 * bBox.height - workspaceDiv.offsetHeight) + 'px';
+    workspaceDiv.style.width = bBox.width + 'px';
+    workspaceDiv.style.width = (2 * bBox.width - workspaceDiv.offsetWidth) + 'px';
+  };
+  window.addEventListener('resize', onresize, false);
+
   Typed.workspace = Blockly.inject(document.getElementById('blocklyDiv'),
       {path: '../../', toolbox: document.getElementById('toolbox'),
        grid:
@@ -21,7 +36,27 @@ Typed.init = function() {
        collapse: false,
        typedVersion: true
       });
+  onresize();
+  Blockly.svgResize(Typed.workspace);
 };
+
+Typed.getBBox_ = function(element) {
+  var height = element.offsetHeight;
+  var width = element.offsetWidth;
+  var x = 0;
+  var y = 0;
+  do {
+    x += element.offsetLeft;
+    y += element.offsetTop;
+    element = element.offsetParent;
+  } while (element);
+  return {
+    height: height,
+    width: width,
+    x: x,
+    y: y
+  };
+}
 
 Typed.showCode = function() {
   // Generate TypedLang code and display it.
