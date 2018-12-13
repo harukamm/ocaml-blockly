f7c2b32e55f352bdc2f193f21ed6c83e2b1b7329
Haruka Matsumoto <harukam0416@gmail.com>
2018-12-13 11:02:39 +0900

    Add new file for AST for blocks and type inference

---
+++ core/type_infer.js
@@ -0,0 +1,17 @@
+/**
+ * @fileoverview Object representing AST for typed blocks, and utility
+ * methods for type inference.
+ * @author harukam0416@gmail.com (Haruka Matsumoto)
+ */
+'use strict';
+
+goog.provide('Blockly.TypeInfer');
+goog.provide('Blockly.TypeInfer.Exp');
+
+goog.require('goog.asserts');
+
+/**
+ * @constructor
+ */
+Blockly.TypeInfer.Exp = function() {
+};
