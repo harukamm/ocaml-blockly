a9232a7554691c03cc117c5cb69d635b3e17a65b
Haruka Matsumoto <harukam0416@gmail.com>
2018-12-14 12:45:30 +0900

    Fix: bound variable names could have duplicates
    in type scheme class

--- core/type_infer.js
+++ core/type_infer.js
@@ -35,8 +35,9 @@ Blockly.Scheme.create = function(env, type) {
   for (var i = 0, tvar; tvar = ftvInType[i]; i++) {
     goog.asserts.assert(!tvar.val,
         'Includes a type which is not a type variable');
-    if (ftvInEnv.indexOf(tvar) == -1) {
-      boundVarNames.push(tvar.name);
+    var name = tvar.name;
+    if (ftvInEnv.indexOf(tvar) == -1 && boundVarNames.indexOf(name) == -1) {
+      boundVarNames.push(name);
     }
   }
   return new Blockly.Scheme(boundVarNames, type);
