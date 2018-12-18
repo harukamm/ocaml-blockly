3cbde39c01a07c1fc285820d015bb51a0eb2116a
Haruka Matsumoto <harukam0416@gmail.com>
2018-12-18 13:39:18 +0900

    Show error message with user input code
    in demos/generator/typed
    
    - There are 4 errors:
      1. AST parsing error
      2. Not supported AST error
      3. Generated XML parsing error
      4. Undefined variable reference error.
      5. Type error
    - If error occurs and blocks are already built up (case 4 or 5),
      disable type/variable checks and dispose of blocks.

--- demos/generator/typed.js
+++ demos/generator/typed.js
@@ -82,13 +82,31 @@ Typed.onClickConvert = function(event) {
 }
 
 Typed.codeToBlock = function(code) {
-  var xmlStr = blockOfOCaml(code);
   try {
-    Typed.fromXMLString(xmlStr);
+    var xmlStr = blockOfOCaml(code);
   } catch (e) {
-    console.log("Conveted: \n" + xmlStr);
-    console.log(e);
+    alert ("Syntax error or not-supported AST error.");
+    return;
   }
+  var result = Typed.fromXMLString(xmlStr);
+  if (result.errMsg) {
+    alert(result.errMsg);
+  }
+}
+
+Typed.disableTypeCheck = function(block) {
+  var desc = block.getDescendants();
+  for (var i = 0, child; child = desc[i]; i++) {
+    var connections = child.getConnections_();
+    for (var j = 0, conn; conn = connections[j]; j++) {
+      conn.disableTypeCheck(true);
+    }
+  }
+}
+
+Typed.forceDispose = function(block) {
+  Typed.disableTypeCheck(block);
+  block.dispose();
 }
 
 Typed.fromXMLString = function(str, opt_workspace) {
@@ -96,13 +114,37 @@ Typed.fromXMLString = function(str, opt_workspace) {
   var parser = new DOMParser();
   var xml = parser.parseFromString(str, "text/xml");
   var parseError = xml.getElementsByTagName("parsererror").length != 0;
+  var result = {errMsg:null, block:null};
   if (parseError) {
-    console.log(xml);
-    throw "Parser error";
-  } else {
-    var block = Blockly.Xml.domToBlock(xml.children[0], workspace);
-    return block;
+    result.errMsg = "XML parsing error";
+    return result;
   }
+  try {
+    var block = Blockly.Xml.domToBlock(xml.children[0], workspace, true);
+  } catch (e) {
+    result.errMsg = "Illegal block XML.";
+    return result;
+  }
+  var resolved = block.resolveReference(null, true);
+  if (!resolved) {
+    result.errMsg = "Undefined varaible.";
+    Typed.forceDispose(block);
+    return result;
+  }
+  var typeCheck = true;
+  try {
+    block.updateTypeInference();
+  } catch (e) {
+    typeCheck = false;
+  }
+  if (!typeCheck) {
+    result.errMsg = "Type error.";
+    Typed.forceDispose(block);
+    return result;
+  }
+  block.workspace.renderTypeChangedWorkspaces();
+  result.block = block;
+  return result;
 }
 
 window.addEventListener('load', Typed.init);
