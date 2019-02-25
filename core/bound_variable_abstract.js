488801ce6cfef734d8911af7e38a238a2b1f7e89
Haruka Matsumoto <harukam0416@gmail.com>
2019-02-25 18:51:56 +0900

    Remove sub fields from variable abstract class
    This reverts commit 2b59d5140c1f7bdb6213551a3d1728707ab3d226 and
    7119d84d8106bcaf6f13d3f37fe1f1247d8c33da.

--- core/bound_variable_abstract.js
+++ core/bound_variable_abstract.js
@@ -53,13 +53,6 @@ Blockly.BoundVariableAbstract = function(block, fieldName, typeExpr, label) {
   this.label = label;
 
   /**
-   * The list of sub field names which the variable is bound to.
-   * @type {!Array.<string>}
-   * @private
-   */
-  this.subFieldNames_ = [];
-
-  /**
    * A unique id for the variable.
    * @type {string}
    * @private
@@ -97,45 +90,19 @@ Blockly.BoundVariableAbstract.prototype.getWorkspace = function() {
 };
 
 /**
- * Functions to return the name of the field this variable is bound to.
+ * Returns the name of the main field this variable is bound to.
  * @return {!string} The name of this variable's field.
  */
 Blockly.BoundVariableAbstract.prototype.getMainFieldName = function() {
   return this.mainFieldName_;
 };
-Blockly.BoundVariableAbstract.prototype.getSubFieldName = function(index) {
-  return this.subFieldNames_[index];
-};
 
 /**
- * Functions to return the field this variable is bound to.
+ * Returns the field this variable is bound to.
  * @return {!Blockly.FieldBoundVariable} This variable's field.
  */
 Blockly.BoundVariableAbstract.prototype.getMainField = function() {
   return this.sourceBlock_.getField(this.mainFieldName_);
-};
-Blockly.BoundVariableAbstract.prototype.getSubField = function(index) {
-  var name = this.getSubFieldName(index);
-  if (name) {
-    return this.sourceBlock_.getField(name);
-  }
-  throw 'The sub field index is out of bounds.';
-};
-
-/**
- * Functions to modify this variable's sub fields.
- */
-Blockly.BoundVariableAbstract.prototype.addSubField = function(name) {
-  if (!name) {
-    throw 'The sub field name can not be empty.';
-  }
-  if (this.subFieldNames_.indexOf(name) != -1) {
-    throw 'The sub field name must be unique.';
-  }
-  this.subFieldNames_.push(name);
-};
-Blockly.BoundVariableAbstract.prototype.removeSubField = function(name) {
-  goog.array.removeLast(this.subFieldNames_, name);
 };
 
 /**
