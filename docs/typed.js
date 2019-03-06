a3fd006535ac0f0d7b36db95443db072aacffdbe
Haruka Matsumoto <harukam0416@gmail.com>
2019-03-07 08:59:09 +0900

    Break default code for demos/typed

--- docs/typed.js
+++ docs/typed.js
@@ -56,8 +56,10 @@ Typed.workspace = null;
 
 Typed.defaultCode =
     "let rec pi_impl n d =" +
-    "if n > 0.0 then n *. n /. d /. (d -. 2.0) *. pi_impl (n -. 2.0) (d -. 2.0) else 1.0;;" +
-    "let pi n = 2.0 *. pi_impl (n *. 2.0) (n *. 2.0 +. 1.0);;" +
+    "if n > 0.0 " +
+    "then n *. n /. d /. (d -. 2.0) *. pi_impl (n -. 2.0) (d -. 2.0) " +
+    "else 1.0;;\n" +
+    "let pi n = 2.0 *. pi_impl (n *. 2.0) (n *. 2.0 +. 1.0);;\n" +
     "let pi_exp = pi 9000.";
 
 Typed.init = function() {
